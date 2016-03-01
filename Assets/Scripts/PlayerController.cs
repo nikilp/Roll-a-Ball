5889b808cd3a0fb975393a399ad54e901ff3f887
nikilp <nikilp@gmail.com>
2016-03-01 12:26:44 +0800

    Initial commit
    This commit contains the source of the Roll-a-Ball game, as described step-by-step in this tutorial: https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+	// Update is called once per frame
+    // Called before rendering a frame - this is where most of the came code should be
+//    void Update()
+//    {
+//		
+//    }
+    
+    // This is called just before preforming any physics calculations
+    void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+    }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+		if (count >= 12)
+			winText.text = "You Won!!!";
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+	}
+}
